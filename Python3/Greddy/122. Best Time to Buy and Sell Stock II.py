e2b4240f35f85d11f8612463a259a1cb363daf49
WatsonWangZh <wang.zh@pku.edu.cn>
2019-06-29 00:52:01 +0800

    122.done 贪心需专题整理

--- Python3/Greddy/122. Best Time to Buy and Sell Stock II.py
+++ Python3/Greddy/122. Best Time to Buy and Sell Stock II.py
@@ -24,11 +24,41 @@
 # Explanation: In this case, no transaction is done, i.e. max profit = 0.
 
 class Solution:
+
+
     def maxProfit(self, prices):
-        pass
+        maxprofit = 0
+        for i in range(len(prices)-1):
+            if prices[i+1] > prices[i]:
+                maxprofit += prices[i+1] - prices[i]
+        return maxprofit
+
+    # 错误解法:
+    # def maxProfit(self, prices):
+    #     maxprofit = 0
+    #     for i in range(len(prices)):
+    #         for j in range(i+1,len(prices)):
+    #             temppro = self.helper(prices[:i]) + self.helper(prices[i:j+1]) + self.helper(prices[j:])
+    #         if  temppro > maxprofit:
+    #             maxprofit = temppro
+    #     return maxprofit
     
+    # def helper(self, prices):
+    #     maxProfit = 0
+    #     minPrice = float('inf')
+
+    #     for p in prices:
+    #         minPrice = min(p, minPrice)
+    #         profit = p - minPrice
+    #         if profit > maxProfit:
+    #             maxProfit = profit
+    #     return maxProfit
+
 def main():
-    pass
+    s = Solution()
+    print(s.maxProfit([7,1,5,3,6,4]))
+    print(s.maxProfit([1,2,3,4,5]))
+    print(s.maxProfit([7,6,4,3,1]))
 
 if __name__ == "__main__":
     main()
