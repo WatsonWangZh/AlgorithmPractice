eba0c8f1e9e79fbc4e6c8ed7068c96b4f073052e
WatsonWangZh <wang.zh@pku.edu.cn>
2019-07-08 22:17:03 +0800

    4 Done.好题

--- Python3/DevideAndConquer/4. Median of Two Sorted Arrays.py
+++ Python3/DevideAndConquer/4. Median of Two Sorted Arrays.py
@@ -15,10 +15,33 @@
 
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        pass
+        A, B, m, n = nums1, nums2, len(nums1), len(nums2)
+        if m > n: A, B, m, n = B, A, n, m
+        if n == 0: raise ValueError
         
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
+        imin, imax, half_len = 0, m, int((m+n+1)/2)
+        
+        while imin <= imax:
+            i = int((imin + imax) / 2)
+            j = half_len - i
+            
+            if i < m and B[j-1] > A[i]:
+                # i is too small, must increase it
+                imin = i+1
+            elif i > 0  and A[i-1] > B[j]:
+                # i is too big, must decrease it
+                imax = i-1
+            else:
+                # i is perfect
+                if i == 0: max_of_left = B[j-1]
+                elif j == 0: max_of_left = A[i-1]
+                else: max_of_left = max(B[j-1], A[i-1])
+                
+                if (m+n)%2 == 1:
+                    return max_of_left
+                
+                if i == m: min_of_right = B[j]
+                elif j == n: min_of_right = A[i]
+                else: min_of_right = min(B[j], A[i])
+                
+                return (max_of_left+min_of_right)/2.0
