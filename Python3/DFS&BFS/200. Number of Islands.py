f91a009fbcad9ad2c75b303c544a05c9cd3b03e0
WatsonWangZh <wang.zh@pku.edu.cn>
2019-07-12 12:19:17 +0800

    200 Number of Islands Done.

--- Python3/DFS&BFS/200. Number of Islands.py
+++ Python3/DFS&BFS/200. Number of Islands.py
@@ -20,11 +20,32 @@
 # Output: 3
 
 class Solution:
-    def numIslands(self, grid):
-        pass
-
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
+    def convertIsland(self, row, col, grid):
+        grid[row][col] = '0'
+        if(col < len(grid[0])-1 and grid[row][col+1] == '1'):
+            self.convertIsland(row, col+1, grid)
+        if(row < len(grid)-1 and grid[row+1][col] == '1'):
+            self.convertIsland(row+1, col, grid)
+        if(row > 0 and grid[row-1][col] == '1'):
+            self.convertIsland(row-1, col, grid)
+        if(col > 0 and grid[row][col-1] == '1'):
+            self.convertIsland(row, col-1, grid)
+        return 
+    
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # When you hit a '1', go through all the island and convert values to 0.
+        # At end, this is 1 island, and then continue to find other '1's that haven't been zeroed out because they weren't in the island.
+        
+        if(grid == []):
+            return 0
+        row, col = 0, 0 
+        maxRow, maxCol = len(grid),len(grid[0]) 
+        count = 0
+        
+        for row in range(maxRow):
+            for col in range(maxCol):
+                if(grid[row][col] == '1'):
+                    count += 1
+                    self.convertIsland(row, col, grid)  
+        return count
+        
