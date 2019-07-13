d68114ce515775005e7d8e5d88c374490f4bc8a1
WatsonWangZh <wang.zh@pku.edu.cn>
2019-07-13 15:21:28 +0800

    846 Hands of Straights Done.

--- Python3/Map/846. Hand of Straights.py
+++ Python3/Map/846. Hand of Straights.py
@@ -18,12 +18,33 @@
 # 0 <= hand[i] <= 10^9
 # 1 <= W <= hand.length
 
+import collections
 class Solution:
+    # 这道题说是我们在打扑克牌，是否能将手里的牌都以顺子的形式出完。
+    # 由于牌可以重复，所以我们要统计每张牌出现的次数，同时还要给牌按大小排序，
+    # 那么用Counter来建立牌的大小和其出现次数之间的映射就最好不过了，
+    # 利用了其可以按key值排序的特点。首先遍历手中牌，建立映射。
+    # 然后开始循环，条件是Counter不为空，然后取出最小的那张牌，然后遍历能组成顺子的W张牌，
+    # 若没有直接返回False，有的话，则映射值自减1，若映射值为0了，则从Counter中移除该映射对即可，
+    # 循环退出后返回True。
+
     def isNStraightHand(self, hand, W):
-        pass
+        if not hand or len(hand) % W:
+            return False
+        
+        counter = collections.Counter(hand)
+        while counter:
+            elem = min(counter)
+            cnt = 0
+            while cnt < W:
+                if elem not in counter:
+                    return False
+                counter[elem] -= 1
+                if not counter[elem]:
+                    del counter[elem]
+                elem += 1
+                cnt += 1
+                
+        return True
+        
 
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
