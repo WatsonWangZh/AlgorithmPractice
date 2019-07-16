4a0a2771051d6fce2aab59cd7fd03d0c8ea59f97
WatsonWangZh <wang.zh@pku.edu.cn>
2019-07-16 16:35:45 +0800

    307 Range Sum Query - Mutable Done.好题

--- Python3/BinaryIndexTree|SegmentTree/307. Range Sum Query - Mutable.py
+++ Python3/BinaryIndexTree|SegmentTree/307. Range Sum Query - Mutable.py
@@ -14,14 +14,71 @@
 
 class NumArray:
 
+    # M1.Naive Method
+    # Time Limit Exceeded
+    # def __init__(self, nums: List[int]):
+    #     self.nums = nums
+
+    # def update(self, i: int, val: int) -> None:
+    #     self.nums[i] = val
+    
+    # def sumRange(self, i: int, j: int) -> int:
+    #     sum = 0
+    #     for _ in range(i,j+1):
+    #         sum += self.nums[_]
+    #     return sum
+
+    # M2. Sqrt Decomposition O(n)
+    # def __init__(self, nums):
+    #     self.width = int(len(nums)**0.5)    # width of each bin (apart from last)
+    #     self.bin_sums = []                  # sum of each bin
+    #     self.nums = nums
+    #     for i, num in enumerate(nums):
+    #         if i % self.width == 0:         # start a new bin
+    #             self.bin_sums.append(num)
+    #         else:                           # add to last bin
+    #             self.bin_sums[-1] += num
+
+    # def update(self, i, val):
+    #     bin_i = i // self.width
+    #     diff = val - self.nums[i]
+    #     self.bin_sums[bin_i] += diff        # update bin_sums
+    #     self.nums[i] = val                  # update nums
+
+    # def sumRange(self, i, j):
+    #     bin_i, bin_j = i // self.width, j // self.width
+    #     range_sum = sum(self.bin_sums[bin_i:bin_j])         # sum of whole bins 
+    #     range_sum += sum(self.nums[bin_j*self.width:j+1])   # add partial last bin
+    #     range_sum -= sum(self.nums[bin_i*self.width:i])     # subtract partial first bin
+    #     return range_sum
+
+    # M3.FenwickTree(树状树组) O(lgn)
+    # https://www.youtube.com/watch?v=WbafSgetDDk
     def __init__(self, nums: List[int]):
-        pass
+        self.array = [0] + nums
+        self.nums = nums
+        for i in range(1, len(self.array)):
+            i2 = i + (i & -i)  #lowbit(i)
+            if i2 < len(self.array):
+                self.array[i2] += self.array[i]
 
     def update(self, i: int, val: int) -> None:
-        pass
+        i += 1
+        diff, self.nums[i-1] = val - self.nums[i-1], val
+        while i < len(self.array):
+            self.array[i] += diff
+            i += (i & -i) #lowbit(i)
+    
+    def prefix_sum(self, idx):
+        idx += 1
+        result = 0
+        while idx:
+            result += self.array[idx]
+            idx -= idx & -idx
 
     def sumRange(self, i: int, j: int) -> int:
-        pass
+        return self.prefix_sum(j) - self.prefix_sum(i-1)
+        
 
 
 # Your NumArray object will be instantiated and called as such:
