95576c0d7b1a00b7c0831c2ee241b6b12ca1e1c4
WatsonWangZh <wang.zh@pku.edu.cn>
2019-07-20 16:58:57 +0800

    34 Find First and Last Position of Element in Sorted Array Done

--- Python3/BinarySearch/374. Guess Number Higher or Lower.py
+++ Python3/BinarySearch/374. Guess Number Higher or Lower.py
@@ -13,7 +13,8 @@
 # The guess API is already defined for you.
 # @param num, your guess
 # @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
-# def guess(num):
+def guess(num):
+    pass
 
 class Solution(object):
     def guessNumber(self, n):
