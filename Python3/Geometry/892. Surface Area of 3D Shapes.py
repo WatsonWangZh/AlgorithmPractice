c0f8d786885dcaef4698d524ef72cdf3e5d6a12c
WatsonWangZh <wang.zh@pku.edu.cn>
2019-07-22 16:31:13 +0800

    892 Surface Area of 3D Shapes Done

--- Python3/Geometry/892. Surface Area of 3D Shapes.py
+++ Python3/Geometry/892. Surface Area of 3D Shapes.py
@@ -24,10 +24,14 @@
 
 class Solution:
     def surfaceArea(self, grid):
-        pass
-
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
+        n = len(grid)
+        res = 0
+        for i in range(grid):
+            for j in range(grid):
+                if grid[i][j] > 0:
+                    res += 4 * grid[i][j] + 2
+                if i > 0:
+                    res -= min(grid[i][j], grid[i - 1][j]) * 2
+                if j > 0:
+                    res -= min(grid[i][j], grid[i][j - 1]) * 2
+        return res
