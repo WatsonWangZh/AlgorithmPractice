aa180311898c2a423766a34c04d5e467bd3d353b
WatsonWangZh <wang.zh@pku.edu.cn>
2019-07-22 16:12:09 +0800

    133 Clone Graph Done

--- Python3/Graph/133. Clone Graph.py
+++ Python3/Graph/133. Clone Graph.py
@@ -17,19 +17,35 @@
 # Since the graph is undirected, if node p has node q as neighbor, then node q must have node p as neighbor too.
 # You must return the copy of the given node as a reference to the cloned graph.
 
-"""
 # Definition for a Node.
 class Node:
     def __init__(self, val, neighbors):
         self.val = val
         self.neighbors = neighbors
-"""
+
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        pass
-
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
+        # 因为图中可能存在环，所以直接将节点和它的相邻节点进行复制，
+        # 并对它的相邻节点进行相同操作可能会进入死循环。
+        # 为了避免循环访问，要对已经复制过的节点进行缓存，
+        # 我们通过一个由标志和节点组成的字典来记录已经访问过的节点。
+        # 当我们通过相邻关系来访问一个节点时，如果它是第一次被访问，
+        # 则要将其加入一个栈中，在栈中的元素表示要继续访问它相邻的元素，
+        # 并记录它已经被访问过，同时要跟新已经被访问过的节点中与其相邻的节点的邻居列表。
+        # 当栈为空时，表示所有的节点都已经访问完毕，图也复制成功。
+        d = {}
+        stack = [node]
+        visited = set()
+        while stack:
+            u = stack.pop()
+            if u not in d:
+                d[u] = Node(u.val, [])
+            if u in visited:
+                continue
+            visited.add(u)
+            for neighbor in u.neighbors:
+                if neighbor not in d:
+                    d[neighbor] = Node(neighbor.val, [])
+                d[u].neighbors.append(d[neighbor])
+                stack.append(neighbor)
+        return d[node]
