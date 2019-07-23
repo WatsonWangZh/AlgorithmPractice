de0671842fc50bc734fc36541ebf7a017348575e
WatsonWangZh <wang.zh@pku.edu.cn>
2019-07-23 14:50:44 +0800

    24 Swap Nodes in Pairs Recommit

--- Python3/LinkedList/24. Swap Nodes in Pairs.py
+++ Python3/LinkedList/24. Swap Nodes in Pairs.py
@@ -14,40 +14,15 @@ class Solution(object):
     def swapPairs(self, head: ListNode) -> ListNode:
         if not head or not head.next:
             return head
-        pre = new_head = ListNode(0)
-        while head and head.next:
-            tmp = head.next
-            head.next = tmp.next
-            tmp.next = head
-            pre.next = tmp
-            pre = head
-            head = head.next
-        return new_head.next
+        dummy = ListNode(0)
+        dummy.next = head
+        cur = dummy
 
-def get_node_chain(expression):
-    expression_numbers = expression.split('->')
-    last_node, first_node = None, None
-    for expression_number in expression_numbers:
-        node = ListNode(int(expression_number))
-        if not last_node:
-            first_node = node
-        if last_node:
-            last_node.next = node
-        last_node = node
-    return first_node
-
-def output_node_chain(node):
-    result = []
-    while node:
-        result.append(str(node.val))
-        node = node.next
-    return result
-
-def main():
-    s = Solution()
-    input_list = "1->2->3->4"
-    res_list = s.swapPairs(get_node_chain(input_list))
-    print("->".join(output_node_chain(res_list)))
-
-if __name__ == "__main__":
-    main()
+        while cur.next and cur.next.next:
+            first = cur.next
+            second = cur.next.next
+            cur.next = second
+            first.next = second.next
+            second.next = first
+            cur = cur.next.next
+        return dummy.next
