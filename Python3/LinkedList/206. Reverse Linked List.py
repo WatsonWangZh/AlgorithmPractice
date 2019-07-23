2527212826f67578c8fe1c02ae4b27cad2f8cfb8
WatsonWangZh <wang.zh@pku.edu.cn>
2019-07-23 15:42:07 +0800

    206 Reverse Linked List Recommit

--- Python3/LinkedList/206. Reverse Linked List.py
+++ Python3/LinkedList/206. Reverse Linked List.py
@@ -20,51 +20,12 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        #wrong answer
-        # p = head
-        # while p:
-        #     q = p.next
-        #     p.next = q.next
-        #     head.next = q
-        #     q.next = p
-        # p.next = head
-        # head = head.next
-        # p.next.next = None
-        # return head
-        
-        lastNode = None
-        while head:
-            nextNode = head.next
-            head.next = lastNode
-            lastNode = head
-            head = nextNode
-        
-        return lastNode.next
-
-def get_node_chain(expression):
-    expression_numbers = expression.split('->')
-    last_node, first_node = None, None
-    for expression_number in expression_numbers:
-        node = ListNode(str(expression_number))
-        if not last_node:
-            first_node = node
-        if last_node:
-            last_node.next = node
-        last_node = node
-    return first_node
-
-def output_node_chain(node):
-    result = []
-    while node:
-        result.append(node.val)
-        node = node.next
-    return result
-
-def main():
-    s = Solution()
-    input_list = "5->4->3->2->1->NULL"
-    res_input = s.reverseList(get_node_chain(input_list))
-    print("->".join(output_node_chain(res_input)))
-
-if __name__ == "__main__":
-    main()
+        if not head or not head.next:
+            return head
+        first, second = head, head.next
+        while second:
+            tmp = second.next
+            second.next = first
+            first, second = second, tmp
+        head.next = None
+        return first
