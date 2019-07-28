75d36b3f621bd6b57f9aee811c9680055f33d45a
WatsonWangZh <wang.zh@pku.edu.cn>
2019-07-28 22:41:38 +0800

    90 Subsets II Recommit

--- Python3/BackTracking/90. Subsets II.py
+++ Python3/BackTracking/90. Subsets II.py
@@ -13,26 +13,46 @@
 #   [1,2],
 #   []
 # ]
-import collections
+
 class Solution(object):
+    # M1.递归
+    # DFS，在78题基础上将nums排序。然后判断当前元素nums[i]是否和nums[i-1]相等，若相等，跳出此次循环继续下一次循环。
+    # def subsetsWithDup(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: List[List[int]]
+    #     """
+    #     nums.sort()
+    #     res = [[]]
+    #     self.dfs(nums, [], res)
+    #     return res
+    
+    # def dfs(self, nums, temp, res):
+    #     if not nums:
+    #         return
+    #     for i in range(len(nums)):
+    #         if i > 0 and nums[i] == nums[i-1]:
+    #             continue
+    #         temp.append(nums[i])
+    #         res.append(temp[:])
+    #         self.dfs(nums[i+1:], temp, res)
+    #         temp.pop()
+
+    # M2.非递归
+    # 类似于层次添加元素[] → [1] → [2], [1, 2] → [1,2], [2,2]....
+    # 若当前元素和上一个元素相等，则本轮 应该从[添加上一个元素]之后的位置添加该元素。
     def subsetsWithDup(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        res = [[]]
         nums.sort()
-        self.helper(nums, res, [], 0)
+        res = [[]]
+        for i in range(len(nums)):
+            if i == 0 or nums[i] != nums[i-1]:
+                l = len(res)
+            for path in res[len(res)-l:]:
+                res.append(path + [nums[i]])
         return res
-    
-    def helper(self, nums, temp, res, startIdx):
-        if not nums:
-            return
-        # res.append(temp[:])
-        for i in range(startIdx, len(nums)):
-            if i != startIdx and nums[i] == nums[i-1]:
-                continue
-            res.append(temp[:])
-            temp.append(nums[i])
-            self.helper(nums, temp, res, i + 1)
-            temp.pop()
+
+
