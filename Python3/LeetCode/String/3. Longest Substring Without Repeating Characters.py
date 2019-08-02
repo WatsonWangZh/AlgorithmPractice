1ed8e09b500b9d426e74340a673fd3cb9c12348c
WatsonWangZh <wang.zh@pku.edu.cn>
2019-08-02 09:22:52 +0800

    LeetCode 3 Longest Substring Without Repeating Characters Redo

--- Python3/LeetCode/String/3. Longest Substring Without Repeating Characters.py
+++ Python3/LeetCode/String/3. Longest Substring Without Repeating Characters.py
@@ -16,26 +16,42 @@
 # Explanation: The answer is "wke", with the length of 3. 
 # Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
+from collections import deque
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-            longest, start, visited = 0, 0, [False for _ in range(256)]
-            for i, char in enumerate(s):
-                if visited[ord(char)]:
-                    while char != s[start]:
-                        visited[ord(s[start])] = False
-                        start += 1
-                    start += 1
+
+            # M1. HashTable
+            # longest, start, visited = 0, 0, [False for _ in range(256)]
+            # for i, char in enumerate(s):
+            #     if visited[ord(char)]:
+            #         while char != s[start]:
+            #             visited[ord(s[start])] = False
+            #             start += 1
+            #         start += 1
+            #     else:
+            #         visited[ord(char)] = True
+            #     longest = max(longest, i - start + 1)
+            # return longest
+
+            # if not s:
+            # return 0
+
+            # M2. 双端队列（deque，全名double-ended queue）
+            if not s:
+                return 0
+
+            maxlen = 1
+            rec = deque()
+            rec.append(s[0])
+
+            for c in s[1:]:
+                if c in rec:
+                    maxlen = max(maxlen, len(rec))
+                    watch = rec.popleft()
+                    while(watch!=c):
+                        watch = rec.popleft()
+                    rec.append(c)
                 else:
-                    visited[ord(char)] = True
-                longest = max(longest, i - start + 1)
-            return longest
-
-def main():
-    s = Solution()
-    print(s.lengthOfLongestSubstring("abcabcbb"))
-    print(s.lengthOfLongestSubstring("bbbbb"))
-    print(s.lengthOfLongestSubstring("pwwkew"))
-
-if __name__ == "__main__":
-    main()
-
+                    rec.append(c)
+            maxlen = max(maxlen, len(rec))
+            return maxlen
