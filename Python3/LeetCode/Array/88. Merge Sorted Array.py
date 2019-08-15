d91b585bfa20dc06851ec697c391758f11c0d440
WatsonWangZh <wang.zh@pku.edu.cn>
2019-08-15 09:54:27 +0800

    LeetCode 88 Merge Sorted Array Done

--- Python3/LeetCode/Array/88. Merge Sorted Array.py
+++ Python3/LeetCode/Array/88. Merge Sorted Array.py
@@ -19,6 +19,15 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
+        # 线性合并 时间复杂度 O(m+n), 空间复杂度O(1)
+        # 设置cur指针指向合并后的nums1数组(大小为m+n)的最后一个元素，
+        # p指向合并前的nums1数组(大小为m)的最后一个元素，
+        # q指向nums2数组(大小为n)的最后一个元素。
+        # 比较p指向的值和q指向的值，将大的值挪进nums1[cur]。
+        # cur指针往前挪，p或者q指针也相应往前挪。
+        # 循环以上步骤直到p=0或q=0
+        # 若q>=0,将nums2数组剩余的元素挪进nums1。
+        
         while m > 0 and n > 0:
             if nums1[m-1] > nums2[n-1]:
                 nums1[m+n-1] = nums1[m-1]
