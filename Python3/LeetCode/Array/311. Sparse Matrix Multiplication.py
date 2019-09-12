f9c961831cbf5b9942ee22b601e41dd222a110ee
WatsonWangZh <wang.zh@pku.edu.cn>
2019-09-12 18:06:11 +0800

    LeetCode 311 Sparse Matrix Multiplication (Locked) Done

--- Python3/LeetCode/Array/311. Sparse Matrix Multiplication.py
+++ Python3/LeetCode/Array/311. Sparse Matrix Multiplication.py
@@ -24,4 +24,13 @@ class Solution(object):
         :type B: List[List[int]]
         :rtype: List[List[int]]
         """
-        
+        mA,nA,nB = len(A),len(A[0]),len(B[0])
+        res = [[0]*len(B[0]) for _ in range(mA)]
+
+        for i in range(mA):
+            for j in range(nA):
+                if A[i][j]:
+                    for k in range(nB):
+                        res[i][k] += A[i][j]*B[j][k]
+                        
+        return res
