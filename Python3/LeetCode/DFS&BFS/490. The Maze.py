a7461466604cd990e31721c847102b43ef7097b3
WatsonWangZh <wang.zh@pku.edu.cn>
2019-09-15 12:13:32 +0800

    LeetCode 490 The Maze (Locked) Done

---
+++ Python3/LeetCode/DFS&BFS/490. The Maze.py
@@ -0,0 +1,91 @@
+# There is a ball in a maze with empty spaces and walls. 
+# The ball can go through empty spaces by rolling up, down, left or right, 
+# but it won't stop rolling until hitting a wall. 
+# When the ball stops, it could choose the next direction.
+# Given the ball's start position, the destination and the maze, 
+# determine whether the ball could stop at the destination.
+# The maze is represented by a binary 2D array. 
+# 1 means the wall and 0 means the empty space. 
+# You may assume that the borders of the maze are all walls. 
+# The start and destination coordinates are represented by row and column indexes.
+
+# Example 1:
+# Input 1: a maze represented by a 2D array
+# 0 0 1 0 0
+# 0 0 0 0 0
+# 0 0 0 1 0
+# 1 1 0 1 1
+# 0 0 0 0 0
+# Input 2: start coordinate (rowStart, colStart) = (0, 4)
+# Input 3: destination coordinate (rowDest, colDest) = (4, 4)
+# Output: true
+# Explanation: One possible way is : left -> down -> left -> down -> right -> down -> right.
+
+# Example 2:
+# Input 1: a maze represented by a 2D array
+# 0 0 1 0 0
+# 0 0 0 0 0
+# 0 0 0 1 0
+# 1 1 0 1 1
+# 0 0 0 0 0
+# Input 2: start coordinate (rowStart, colStart) = (0, 4)
+# Input 3: destination coordinate (rowDest, colDest) = (3, 2)
+# Output: false
+# Explanation: There is no way for the ball to stop at the destination.
+
+# Note:
+# There is only one ball and one destination in the maze.
+# Both the ball and the destination exist on an empty space, 
+# and they will not be at the same position initially.
+# The given maze does not contain border (like the red rectangle in the example pictures), 
+# but you could assume the border of the maze are all walls.
+# The maze contains at least 2 empty spaces, 
+# and both the width and height of the maze won't exceed 100.
+
+class Solution(object):
+    def hasPath(self, maze, start, destination):
+        """
+        :type maze: List[List[int]]
+        :type start: List[int]
+        :type destination: List[int]
+        :rtype: bool
+        """
+        # DFS 不撞南墙不回头，回头检查终点否
+
+        if not maze or not maze[0]:
+            return False
+        
+        self.m, self.n = len(maze), len(maze[0])
+        visited = [[0] * self.n for _ in range(self.m)]
+        dir = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+        
+        x, y = start[0], start[1]
+
+        return self.dfs(maze, visited, dir, x, y, destination) 
+        
+        
+    def dfs(self, maze, visited, dir, x, y, destination):
+
+        visited[x][y] = 1
+        
+        if x == destination[0] and y == destination[1]:
+            return True
+        
+        for dx, dy in dir:
+            # move along the direction until reach the wall
+            xn, yn = x+dx, y+dy
+            while 0 <= xn < self.m and 0 <= yn < self.n and maze[xn][yn] == 0:
+                xn += dx
+                yn += dy
+            
+            # move back to the last feasbile location
+            xn -= dx
+            yn -= dy
+            
+            if visited[xn][yn]:
+                continue
+                
+            if self.dfs(maze, visited, dir, xn, yn, destination):
+                return True
+            
+        return False
