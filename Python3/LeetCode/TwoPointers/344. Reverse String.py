332dfab9778ba70091fabba7bf61ece8f7a5e7f3
WatsonWangZh <wang.zh@pku.edu.cn>
2019-10-21 19:16:31 +0800

    LeetCode 344 Reverse String Done

---
+++ Python3/LeetCode/TwoPointers/344. Reverse String.py
@@ -0,0 +1,27 @@
+# Write a function that reverses a string. The input string is given as an array of characters char[].
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+# You may assume all the characters consist of printable ascii characters.
+
+# Example 1:
+# Input: ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+
+# Example 2:
+# Input: ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+
+# Hints: 
+# The entire logic for reversing a string is based on using the opposite directional two-pointer approach!
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        l = len(s)
+        for i in range(l//2):
+            tmp = s[l-i-1]
+            s[l-i-1] = s[i]
+            s[i] = tmp
+        return s
