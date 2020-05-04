549a629e49af45cb039de9a9206f011fe3757ab1
WatsonWangZh <wang.zh@pku.edu.cn>
2020-05-04 13:20:15 +0800

    LeetCode 234 Palindrome Linked List

---
+++ Python3/LeetCode/LinkedList/234. Palindrome Linked List.py
@@ -0,0 +1,60 @@
+# Given a singly linked list, determine if it is a palindrome.
+
+# Example 1:
+# Input: 1->2
+# Output: false
+
+# Example 2:
+# Input: 1->2->2->1
+# Output: true
+
+# Follow up:
+# Could you do it in O(n) time and O(1) space?
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+
+        if head is None:
+            return True
+
+        # Find the end of first half and reverse second half.
+        first_half_end = self.end_of_first_half(head)
+        second_half_start = self.reverse_list(first_half_end.next)
+
+        # Check whether or not there's a palindrome.
+        result = True
+        first_position = head
+        second_position = second_half_start
+        while result and second_position is not None:
+            if first_position.val != second_position.val:
+                result = False
+            first_position = first_position.next
+            second_position = second_position.next
+
+        # Restore the list and return the result.
+        first_half_end.next = self.reverse_list(second_half_start)
+        return result    
+
+    def end_of_first_half(self, head):
+        fast = head
+        slow = head
+        while fast.next is not None and fast.next.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+    def reverse_list(self, head):
+        previous = None
+        current = head
+        while current is not None:
+            next_node = current.next
+            current.next = previous
+            previous = current
+            current = next_node
+        return previous
