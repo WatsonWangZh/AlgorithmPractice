196a662533255d7d6861e8d0a04bf89a5f98bed0
WatsonWangZh <wang.zh@pku.edu.cn>
2020-05-31 21:41:41 +0800

    LeetCode 72 Edit Distance Redo

--- Python3/LeetCode/DynamicProgramming/72. Edit Distance.py
+++ Python3/LeetCode/DynamicProgramming/72. Edit Distance.py
@@ -55,3 +55,31 @@ class Solution(object):
                 matrix[i][j] = min(matrix[i - 1][j] + 1, matrix[i][j - 1] + 1, matrix[i - 1][j - 1] + cost)
         return matrix[lenth1 - 1][lenth2 - 1]
         
+        # 二刷
+        n = len(word1)
+        m = len(word2)
+        
+        # if one of the strings is empty
+        if n * m == 0:
+            return n + m
+        
+        # array to store the convertion history
+        d = [ [0] * (m + 1) for _ in range(n + 1)]
+        
+        # init boundaries
+        for i in range(n + 1):
+            d[i][0] = i
+        for j in range(m + 1):
+            d[0][j] = j
+        
+        # DP compute 
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                left = d[i - 1][j] + 1
+                down = d[i][j - 1] + 1
+                left_down = d[i - 1][j - 1] 
+                if word1[i - 1] != word2[j - 1]:
+                    left_down += 1
+                d[i][j] = min(left, down, left_down)
+        
+        return d[n][m]
