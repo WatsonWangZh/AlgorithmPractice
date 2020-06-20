2aefb11618548595f6ef56586c47840c7ff1c920
WatsonWangZh <wang.zh@pku.edu.cn>
2020-06-20 16:27:12 +0800

    LeetCode 60 Permutation Sequence Redo

--- Python3/LeetCode/Math/60. Permutation Sequence.py
+++ Python3/LeetCode/Math/60. Permutation Sequence.py
@@ -26,7 +26,7 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        # 规律计数  实现技巧 O(n^2)O
+        # 规律计数  实现技巧 O(n^2)
         # 做法：
         # 从高位到低位依次考虑每一位；
         # 对于每一位，从小到大依次枚举未使用过的数，确定当前位是几；
@@ -58,3 +58,81 @@ class Solution(object):
             k = r
             n -= 1
         return res
+
+# Solution based on Leetcode 46. Permutations, Got TLE. 
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        
+        return ''.join(self.permute([str(i) for i in range(1, n+1)])[k-1])
+    
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        temp = []
+        visited = [False] * len(nums)
+        self.dfs(nums, 0, temp, res, visited)
+        return res
+
+    def dfs(self, nums, pos, temp, res, visited):
+        if pos == len(nums):
+            res.append(temp[:])
+            return 
+
+        for i in range(len(nums)):
+            if not visited[i]:
+                visited[i] = True
+                temp.append(nums[i])
+                self.dfs(nums, pos+1, temp, res, visited)
+                visited[i] = False
+                temp.pop()
+
+# Solution based on Leetcode 31. next Permutation
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        import math
+        k = k % (math.factorial(n)+1)
+        nums = [i+1 for i in range(n)]  # 1st permutation of nums
+        for i in range(k-1):
+            nums = self.nextPermutation(nums)
+        return "".join([ str(ch) for ch in nums])
+
+    def nextPermutation(self, nums):
+
+        n = len(nums)
+        i,j = n-1, n-1
+
+        # From right to left, find the first decreasing number
+        while i > 0 and nums[i] <= nums[i-1]:
+            i -= 1 
+        
+        # [3,2,1] -> [1,2,3]
+        if i==0: 
+            nums[:]=nums[::-1]
+            return nums
+
+        # From right to left, finding number just larget than the first decreasing number
+        for j in range(n-1, i-1, -1):
+            if nums[j] > nums[i-1]:
+                break 
+             
+        # Swap 
+        nums[j], nums[i-1] = nums[i-1], nums[j]
+
+        # Reversing the backend numbers
+        nums[i:] = nums[i:][::-1]
+
+        return nums
+    
