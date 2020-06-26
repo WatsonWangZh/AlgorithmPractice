65669e18f99ca6ef25467313b50453dd7d14b061
WatsonWangZh <wang.zh@pku.edu.cn>
2020-06-26 21:12:49 +0800

    LeetCode 129 Sum Root to Leaf Numbers Redo

--- LeetCode/Python3/BinaryTree/129. Sum Root to Leaf Numbers.py
+++ LeetCode/Python3/BinaryTree/129. Sum Root to Leaf Numbers.py
@@ -52,3 +52,92 @@ class Solution(object):
         if not root.left and not root.right:
             return cur_val * 10 + root.val
         return self.helper(root.left, cur_val*10 + root.val) + self.helper(root.right, cur_val*10 + root.val)
+
+# 迭代求解 O(n) O(lgn)
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        res = 0
+        stack = [(root, 0) ]
+        
+        while stack:
+            root, curr_number = stack.pop()
+            if root:
+                curr_number = curr_number * 10 + root.val
+                # if it's a leaf, update root-to-leaf sum
+                if root.left is None and root.right is None:
+                    res += curr_number
+                else:
+                    stack.append((root.right, curr_number))
+                    stack.append((root.left, curr_number))
+                        
+        return res
+
+
+# 先序递归 O(n) O(lgn)
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        res = 0
+        
+        def preorder(root, curr_number):
+            nonlocal res
+            if root:
+                curr_number = curr_number * 10 + root.val
+                
+                # if it's a leaf, update root-to-leaf sum
+                if not root.left and not root.right:
+                    res += curr_number
+                    
+                preorder(root.left, curr_number)
+                preorder(root.right, curr_number)
+                
+        preorder(root, 0)
+        return res
+    
+# Awesome Morris Preorder Traversal! O(n) O(1)
+class Solution:
+    def sumNumbers(self, root: TreeNode):
+        root_to_leaf = curr_number = 0
+        
+        while root:  
+            # If there is a left child,
+            # then compute the predecessor.
+            # If there is no link predecessor.right = root --> set it.
+            # If there is a link predecessor.right = root --> break it.
+            if root.left: 
+                # Predecessor node is one step to the left 
+                # and then to the right till you can.
+                predecessor = root.left 
+                steps = 1
+                while predecessor.right and predecessor.right is not root: 
+                    predecessor = predecessor.right 
+                    steps += 1
+
+                # Set link predecessor.right = root
+                # and go to explore the left subtree
+                if predecessor.right is None:
+                    curr_number = curr_number * 10 + root.val                    
+                    predecessor.right = root  
+                    root = root.left  
+                # Break the link predecessor.right = root
+                # Once the link is broken, 
+                # it's time to change subtree and go to the right
+                else:
+                    # If you're on the leaf, update the sum
+                    if predecessor.left is None:
+                        root_to_leaf += curr_number
+                    # This part of tree is explored, backtrack
+                    for _ in range(steps):
+                        curr_number //= 10
+                    predecessor.right = None
+                    root = root.right 
+                    
+            # If there is no left child
+            # then just go right.        
+            else: 
+                curr_number = curr_number * 10 + root.val
+                # if you're on the leaf, update the sum
+                if root.right is None:
+                    root_to_leaf += curr_number
+                root = root.right
+                        
+        return root_to_leaf
