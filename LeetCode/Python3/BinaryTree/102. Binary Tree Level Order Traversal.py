e7bf92fa6dbd5dee731f1dec1d86f9e69e83a9b6
WatsonWangZh <wang.zh@pku.edu.cn>
2020-07-02 15:56:11 +0800

    LeetCode 102 107 Redo

--- LeetCode/Python3/BinaryTree/102. Binary Tree Level Order Traversal.py
+++ LeetCode/Python3/BinaryTree/102. Binary Tree Level Order Traversal.py
@@ -14,6 +14,8 @@
 #   [15,7]
 # ]
 
+
+# 二刷
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, x):
@@ -48,4 +50,43 @@ class Solution(object):
                 level.append(r.val)
             res.append(level)
         return res
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
         
+        levels = [] 
+        level = 0
+        queue = deque([root])
+        
+        while queue:
+            # start the current level
+            levels.append([])
+            # number of elements in the current level 
+            level_length = len(queue)
+            
+            for i in range(level_length):
+                node = queue.popleft()
+                # fulfill the current level
+                levels[level].append(node.val)
+                
+                # add child nodes of the current level
+                # in the queue for the next level
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            # go to next level
+            level += 1
+        
+        return levels
--- LeetCode/Python3/BinaryTree/107. Binary Tree Level Order Traversal II.py
+++ LeetCode/Python3/BinaryTree/107. Binary Tree Level Order Traversal II.py
@@ -52,3 +52,44 @@ class Solution(object):
             levels.append([node.val for node in level])
             level = [kid for node in level for kid in (node.left, node.right) if kid]
         return levels[::-1]
+
+
+# 二刷
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        
+        levels = [] 
+        level = 0
+        queue = deque([root])
+        
+        while queue:
+            # start the current level
+            levels.append([])
+            # number of elements in the current level 
+            level_length = len(queue)
+            
+            for i in range(level_length):
+                node = queue.popleft()
+                # fulfill the current level
+                levels[level].append(node.val)
+                
+                # add child nodes of the current level
+                # in the queue for the next level
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            # go to next level
+            level += 1
+        
+        return levels[::-1]
