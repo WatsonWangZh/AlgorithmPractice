600d2f6350d12f69e6311fdaf8f47d997c82d8c2
WatsonWangZh <wang.zh@pku.edu.cn>
2020-07-17 16:57:02 +0800

    :clock5: 347 Top K Frequent Elements Redo

--- LeetCode/Algorithms/Stack&PriorityQueue/347. Top K Frequent Elements.py
+++ LeetCode/Algorithms/Stack&PriorityQueue/347. Top K Frequent Elements.py
@@ -12,28 +12,49 @@
 # You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
 # Your algorithm's time complexity must be better than O(n log n), 
 # where n is the array's size.
-import collections
+
+from collections import defaultdict, Counter
 import heapq
+
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # # M1.调包 hash
-        # count = collections.Counter(nums)   
-        # return heapq.nlargest(k, count.keys(), key=count.get)
+        # M1.内置函数
+        count = Counter(nums)
+        return heapq.nlargest(k, count.keys(), key=count.get)
 
-        # M2.手写桶排序
-        cnt, res = collections.defaultdict(int), []
-        for i in nums:
-            cnt[i] += 1
+        # M2.桶排序
+        cnt = defaultdict(int)
+        res = []
+        for num in nums:
+            cnt[num] += 1
 
         bucket = [[] for i in range(max(cnt.values()))]
         for key, value in cnt.items():
             bucket[value-1].append(key)
         
-        for key,value  in enumerate(bucket[::-1]):
+        for key, value in enumerate(bucket[::-1]):
             if len(value):
                 res = res + value
             if len(res) == k:
                 break
 
         return res
-            
+
+        # N3, 最小堆
+        cnt = defaultdict(int)
+        for num in nums:
+            cnt[num] += 1
+
+        q = []
+        for key, value in cnt.items():
+            if len(q) < k:
+                heapq.heappush(q, (value, key))
+            elif value > q[0][0]:
+                heapq.heapreplace(q, (value, key))
+
+        res = []
+        while q:
+            res.append(heapq.heappop(q)[1])
+
+        return res
