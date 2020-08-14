8b3ab8c544a5a46799c76bbc7905f6a630ba9406
WatsonWangZh <wang.zh@pku.edu.cn>
2020-08-14 14:25:10 +0800

    72 Edit Distance

--- LeetCode/Algorithms/DynamicProgramming/72. Edit Distance.py
+++ LeetCode/Algorithms/DynamicProgramming/72. Edit Distance.py
@@ -83,3 +83,24 @@ class Solution(object):
                 d[i][j] = min(left, down, left_down)
         
         return d[n][m]
+
+        # 三刷
+        m, n = len(word1), len(word2)
+        if m == 0:
+            return n
+        if n == 0:
+            return m
+        edit = [[0] *(n+1) for _ in range(m+1)]
+        
+        for i in range(1, m+1):
+            edit[i][0] = i
+        for j in range(1, n+1):
+            edit[0][j] = j
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if word1[i-1] == word2[j-1]:
+                    diff = 0
+                else:
+                    diff = 1
+                edit[i][j] = min(edit[i-1][j-1]+diff, edit[i-1][j]+1, edit[i][j-1]+1)
+        return edit[m][n]
