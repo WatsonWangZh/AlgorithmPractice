094ed14c60b2f8daee27b87ac0dc62e3c9947bde
WatsonWangZh <wang.zh@pku.edu.cn>
2020-09-01 16:37:42 +0800

    157 Read N Characters Given Read4

--- LeetCode/Algorithms/String/157. Read N Characters Given Read4.py
+++ LeetCode/Algorithms/String/157. Read N Characters Given Read4.py
@@ -76,12 +76,16 @@ class Solution(object):
         :type n: Number of characters to read (int)
         :rtype: The number of actual characters read (int)
         """
-        # 这道题相当于说我们每次读取文件里面的所有字符都是先由read4(buf)这个函数去读取，read4每次最多读取4个字符，read4的buf里最多有4个字符
-        # 然后，每次调用read4(buf)这个函数就是直接返回他这一次读取了多少个字符，buf的前n个(n是他的返回值)是他这一次都读入了哪些字符，
+        # 这道题相当于说我们每次读取文件里面的所有字符都是先由read4(buf)这个函数去读取，
+        # read4每次最多读取4个字符，read4的buf里最多有4个字符
+        # 然后，每次调用read4(buf)这个函数就是直接返回他这一次读取了多少个字符，
+        # buf的前n个(n是他的返回值)是他这一次都读入了哪些字符，
         # 所以相当于有两个返回值，读取字符的个数n, 以及这一次读取buf里面装了哪些字符
         # 然后现在要设计一个read函数，他的buf里面一次性最多能装n个字符，然后就是这个n和文件总长度的关系
-        # 但是read函数不能直接接触文件，每次的读入都要借用read4(buf)函数，所以其实read函数是利用read4函数来读取文件（字符串）
-        # 也就是说read函数自己也要返回一个buf,然后用read4的buf拼接成read函数的buf,我们自己也不用声明buf,因为这个函数要自带这个buf
+        # 但是read函数不能直接接触文件，每次的读入都要借用read4(buf)函数，
+        # 所以其实read函数是利用read4函数来读取文件（字符串）
+        # 也就是说read函数自己也要返回一个buf,然后用read4的buf拼接成read函数的buf,
+        # 我们自己也不用声明buf,因为这个函数要自带这个buf
 
         tmp = [""] * 4
         idx = 0
